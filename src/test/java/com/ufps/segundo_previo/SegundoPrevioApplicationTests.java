effa7cb3063fdf6e93145f8a434e03d60b549f86
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-05-29 10:14:40 -0500

    first commit

---
+++ src/test/java/com/ufps/segundo_previo/SegundoPrevioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufps.segundo_previo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoPrevioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
